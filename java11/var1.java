bc5c8ca443bf5d6ed5a6c5879bd98e5de2d32074
Bhavya1407 <91250773+Bhavya1407@users.noreply.github.com>
2022-06-17 23:23:25 +0530

    Create var1.java

---
+++ java11/var1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class var1 {
+	public static void main(String[] args)
+	{
+		// Generic list using var
+		var<var> al = new ArrayList<>();
+			
+		// add elements
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		
+		// print the list
+		System.out.println(al);
+	}
+}
