fff03e930bee3017801b4dd76fff3105c94e3741
Bhavya1407 <91250773+Bhavya1407@users.noreply.github.com>
2022-06-17 23:24:47 +0530

    Create var3.java

---
+++ java11/var3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Demo6 {
+	public static void main(String[] args)
+	{
+
+		// declaration without
+		// initialization
+		var variable;
+		
+		// This is also not valid
+		var variable = null;
+	}
+}
